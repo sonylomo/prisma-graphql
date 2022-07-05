6053f550ff52e28a628bc847bc8504dac90b5aa2
Ubuntu <sonylomo@SonyInSchool.bwrniqtq2i2e1nie2x1gofncya.zx.internal.cloudapp.net>
2022-07-05 08:54:37 +0000

    add user model

--- src/schema.js
+++ src/schema.js
@@ -1,11 +1,20 @@
 const { gql } = require('apollo-server')
+const { prisma } = require('./db')
 
 const typeDefs = gql`
+  type User {
+    email: String!
+    id: ID!
+    name: String
+    posts: [Post!]!
+  }
+
   type Post {
     content: String
     id: ID!
     published: Boolean!
     title: String!
+    author: User
   }
 
   type Query {
@@ -14,64 +23,89 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createDraft(content: String, title: String!): Post!
+    createUser(data: UserCreateInput!): User!
+    createDraft(authorEmail: String, content: String, title: String!): Post!
     publish(id: ID!): Post
   }
+
+  input UserCreateInput {
+    email: String!
+    name: String
+    posts: [PostCreateWithoutAuthorInput!]
+  }
+
+  input PostCreateWithoutAuthorInput {
+    content: String
+    published: Boolean
+    title: String!
+  }
 `
-const posts = [
-  {
-    id: 1,
-    title: 'Subscribe to GraphQL Weekly for community news ',
-    content: 'https://graphqlweekly.com/',
-    published: true,
-  },
-  {
-    id: 2,
-    title: 'Follow DigitalOcean on Twitter',
-    content: 'https://twitter.com/digitalocean',
-    published: true,
-  },
-  {
-    id: 3,
-    title: 'What is GraphQL?',
-    content: 'GraphQL is a query language for APIs',
-    published: false,
-  },
-]
 
 const resolvers = {
   Query: {
     feed: (parent, args) => {
-      return posts.filter((post) => post.published)
+      return prisma.post.findMany({
+        where: { published: false },
+      })
     },
     post: (parent, args) => {
-      return posts.find((post) => post.id === Number(args.id))
+      return prisma.post.findUnique({
+        where: { id: Number(args.id) },
+      })
     },
   },
   Mutation: {
     createDraft: (parent, args) => {
-      posts.push({
-        id: posts.length + 1,
-        title: args.title,
-        content: args.content,
-        published: false,
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+          published: false,
+          author: args.authorEmail && {
+            connect: { email: args.authorEmail },
+          },
+        },
       })
-      return posts[posts.length - 1]
     },
     publish: (parent, args) => {
-      const postToPublish = posts.find((post) => post.id === Number(args.id))
-      postToPublish.published = true
-      return postToPublish
+      return prisma.post.update({
+        where: { id: Number(args.id) },
+        data: {
+          published: true,
+        },
+      })
+    },
+    createUser: (parent, args) => {
+      return prisma.user.create({
+        data: {
+          email: args.data.email,
+          name: args.data.name,
+          posts: {
+            create: args.data.posts,
+          },
+        },
+      })
+    },
+  },
+  User: {
+    posts: (parent, args) => {
+      return prisma.user
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .posts()
     },
   },
   Post: {
-    content: (parent) => parent.content,
-    id: (parent) => parent.id,
-    published: (parent) => parent.published,
-    title: (parent) => parent.title,
+    author: (parent, args) => {
+      return prisma.post
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .author()
+    },
   },
 }
-
 
 module.exports = {
   resolvers,
